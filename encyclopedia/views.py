1bc809f2ad31e5a18f9cf3099f28947c8a86cff6
IMCurie <1835455226@qq.com>
2022-04-02 15:24:09 +0800

    update entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,6 @@
+from email import message
+import django
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +11,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def content(request, content):
+    title = util.get_entry(content)
+
+    if not title:
+        return  render(request, "encyclopedia/apology.html", {
+            "message": "Not_Found"
+        })
+
+    return render(request, "encyclopedia/content.html", {
+        "content" : title
+    })
